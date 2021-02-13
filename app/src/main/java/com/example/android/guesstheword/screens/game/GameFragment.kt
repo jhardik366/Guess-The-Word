572fce768343b6eab1c4ca850ff5b825459f6acd
Hardik Jain <hardic19@gmail.com>
2021-02-14 03:30:59 +0530

    LiveData transformations

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -99,7 +99,7 @@ class GameFragment : Fragment() {
 
     /*** Called when the game is finished */
     private fun gameFinished() {
-        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        Toast.makeText(activity, "Game Over", Toast.LENGTH_SHORT).show()
         val action = GameFragmentDirections.actionGameToScore()
         action.score = viewModel.score.value?:0
         NavHostFragment.findNavController(this).navigate(action)
